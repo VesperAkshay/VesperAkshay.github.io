fbeb2b62c724196639edc3117d49f77adc7953f1
VesperAkshay <5638.akshay@gmail.com>
2026-09-25 17:27:08 +0530

    fix: use accurate Apple logo vector from apple-logo-svgrepo-com.svg

--- src/components/shell/AboutMacModal.tsx
+++ src/components/shell/AboutMacModal.tsx
@@ -25,11 +25,10 @@ export const AboutMacModal: React.FC<AboutMacModalProps> = ({ isOpen, onClose })
           <X className="w-4 h-4" />
         </button>
 
-        {/* Laptop icon / graphic */}
+        {/* Apple Logo graphic */}
         <div className="w-20 h-20 rounded-2xl bg-gradient-to-tr from-slate-700 to-slate-900 border border-white/20 shadow-xl flex items-center justify-center text-white mb-4">
-          <svg className="w-12 h-12 fill-current" viewBox="0 0 24 24">
-            <path d="M4 6h16v10H4z" fillOpacity="0.4" />
-            <path d="M20 18H4V5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v13zm2 1H2a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1h20a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1z" />
+          <svg className="w-10 h-10 fill-current" viewBox="0 0 24 24">
+            <path d="M18.71 19.5C17.88 20.74 17 21.95 15.66 21.97C14.32 22 13.89 21.18 12.37 21.18C10.84 21.18 10.37 21.95 9.09997 22C7.78997 22.05 6.79997 20.68 5.95997 19.47C4.24997 17 2.93997 12.45 4.69997 9.39C5.56997 7.87 7.12997 6.91 8.81997 6.88C10.1 6.86 11.32 7.75 12.11 7.75C12.89 7.75 14.37 6.68 15.92 6.84C16.57 6.87 18.39 7.1 19.56 8.82C19.47 8.88 17.39 10.1 17.41 12.63C17.44 15.65 20.06 16.66 20.09 16.67C20.06 16.74 19.67 18.11 18.71 19.5ZM13 3.5C13.73 2.67 14.94 2.04 15.94 2C16.07 3.17 15.6 4.35 14.9 5.19C14.21 6.04 13.07 6.7 11.95 6.61C11.8 5.46 12.36 4.26 13 3.5Z" />
           </svg>
         </div>
 
